faec7a27ec15f528cf0746d4b5dc6d148aed1c8b
István Szegedi <istvan.szegedi@innonic.com>
2018-06-10 17:17:55 +0200

    my solution

--- src/components/LeftRightSide.vue
+++ src/components/LeftRightSide.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-    <div id="generator-panel" class="generator-panel">
-      <label><b>Choose columns count</b></label>
-      <br>
-      <select v-model="selected">
-        <option disabled value="">Columns count</option>
-        <option v-for="n in selectOptions">{{ n }} </option>
+  <div class="layout">
+    <div class="generator-panel">
+      <h3>
+        Choose columns count
+      </h3>
+      <select class="generator-select" v-model="selected">
+        <option v-for="n in selectOptions" :value="n">{{ n }}</option>
       </select>
     </div>
     <div class="grid-container-wrapper">
-      <b>Grid template will generate here</b>
-      <div class="grid-container" v-bind:style="{gridTemplateColumns: [styleText]}">
-        <template v-for="item in gridArray">
-          <div class="grid-item">{{ item }}</div>
-        </template>
+      <h3>
+        Grid template will generate here
+      </h3>
+      <div class="grid-container">
+        <div v-for="item in grids" class="grid-item">{{ item + 1 }}</div>
       </div>
     </div>
   </div>
@@ -24,24 +24,13 @@
     name: 'LeftRightSide',
     data() {
       return {
-        selected: '',
-        selectOptions: [2,3,4,5,6]
+        selected: 2,
+        selectOptions: [2, 3, 4, 5, 6]
       }
     },
     computed: {
-      styleText: function() {
-        let returnText = '';
-        for(let i = 0; i < this.selected; i++){
-          returnText += " auto";
-        }
-        return returnText;
-      },
-      gridArray: function() {
-        let returnArray = [];
-        for(let i = 1; i <= this.selected; i++){
-          returnArray.push(i);
-        }
-        return returnArray;
+      grids: function () {
+        return [...Array(this.selected).keys()]
       }
     }
   }
@@ -49,22 +38,31 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .generator-panel {
-    width: 20%;
-    float: left;
+  .layout {
+    display: grid;
+    grid-template-columns: 20% 1fr;
+    grid-gap: 30px;
+    padding: 30px;
   }
-  .grid-container-wrapper {
-    width: 80%;
-    float: left;
+
+  .generator-select {
+    width: 100%;
+    cursor: pointer;
+    border-radius: 3px;
+    border: 1px solid #888;
+    padding: 10px;
+    font-size: 16px;
+    font-weight: bold;
   }
 
   .grid-container {
     display: grid;
+    grid-auto-flow: column;
     background-color: #2196F3;
     padding: 10px;
     grid-gap: 1em;
-
   }
+
   .grid-item {
     background-color: rgba(255, 255, 255, 0.8);
     border: 1px solid rgba(0, 0, 0, 0.8);
